31a839919f27895cb304da989a91913549286122
Polo <paul.poitrenaud@gmail.com>
2019-10-14 17:43:18 +0200

    exo maj

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,27 @@
-class Event
+require 'pry'
+require 'time'
 
+class Event
+  attr_accessor :start_date, :duration, :title, :attendees
+
+  def initialize (start_date, duration, title, attendees)
+      @start_date = Time.parse (start_date)
+      @duration, @title, @attendees = duration, title, attendees
+  end
+  
+  def postpone_24h
+    return @start_date += 24*60*60
+  end
+
+  def end_date
+    return @start_date += duration*60
+  end
+  
+  def is_past?
+    return start_date < Time.now
+  end
 end
+
+
+binding.pry
+puts "end of file"
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,18 @@
+require 'pry'
+
 class User
+  attr_accessor :email, :age
+  @@all_user = []
+
+  def initialize(email_to_save, age_to_save)
+    @email = email_to_save
+    @age = age_to_save
+    @@all_user << self
+  end
+
+  def self.all
+      return @@all_user
+  end
+  
 
 end
